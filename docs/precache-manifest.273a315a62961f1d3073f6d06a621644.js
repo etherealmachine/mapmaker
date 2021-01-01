c137d9776fd8e942b1f63982e3fc7ea44bc68165
James Pettit <james@academia.edu>
2021-01-01 10:15:54 -0800

    Build from master

---
+++ docs/precache-manifest.273a315a62961f1d3073f6d06a621644.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "062302c2ebd62a1092b01c5b28a81c14",
+    "url": "/rpg-ai/index.html"
+  },
+  {
+    "revision": "2c5bf7062bf5b18cebd1",
+    "url": "/rpg-ai/static/css/main.4ea84514.chunk.css"
+  },
+  {
+    "revision": "84232c4c015dec720491",
+    "url": "/rpg-ai/static/js/2.3475f91b.chunk.js"
+  },
+  {
+    "revision": "a63e3c6c52fa3bf6c35f0b4d156b3fc9",
+    "url": "/rpg-ai/static/js/2.3475f91b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c5bf7062bf5b18cebd1",
+    "url": "/rpg-ai/static/js/main.1c4b85a1.chunk.js"
+  },
+  {
+    "revision": "d0caa546dedc7c64f134",
+    "url": "/rpg-ai/static/js/runtime-main.0794152b.js"
+  }
+]);
